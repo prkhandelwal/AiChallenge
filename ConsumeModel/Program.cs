e5902837f3ddcfebea2dbd5494274383027cd33c
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-12-19 23:12:34 +0530

    Added consumption Project

---
+++ ConsumeModel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace ConsumeModel
+{
+    class Program
+    {
+        static readonly string _modelPath = Path.Combine("F:","MSFT AI Challenge","Model.zip");
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+        }
+    }
+}
